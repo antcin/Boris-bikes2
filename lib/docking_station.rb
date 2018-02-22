bf329a70d905e49ee19d1299c7053c9b816f5c64
agnesdonat <agi.donat@gmail.com>
2018-02-22 15:48:29 +0000

    Test and created bike method

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -5,6 +5,10 @@ class DockingStation
     Bike.new
   end
 
-  def dock_bike(bike)
+  def dock(bike)
   end
+
+  def bike
+  end
+
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -4,8 +4,8 @@ describe DockingStation do
   # it 'responds to release_bike' do
   #   expect(subject).to respond_to :release_bike
   # end
-  it { is_expected.to respond_to(:release_bike) }
-  it { is_expected.to respond_to(:dock_bike).with(1).argument }
+  it { is_expected.to respond_to(:release_bike, :bike) }
+  it { is_expected.to respond_to(:dock).with(1).argument }
 
   it 'should release a bike' do
   bike = subject.release_bike
